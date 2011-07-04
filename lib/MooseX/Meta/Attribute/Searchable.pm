2c47e61fd177bb954333cc151de36466f008ae9f
Cory G Watson <gphat@onemogin.com>
2011-07-04 13:42:20 -0500

    Add tests and flesh things out.

--- lib/MooseX/Meta/Attribute/Searchable.pm
+++ lib/MooseX/Meta/Attribute/Searchable.pm
@@ -1,5 +1,77 @@
-use strict;
-use warnings;
 package MooseX::Meta::Attribute::Searchable;
+use Moose::Role;
 
+# ABSTRACT: Make an attribute searchable for an external index.
+
+=head1 SYNOPSIS
+
+    package MyObject;
+    use Moose;
+
+    with 'MooseX::Role::Searchable';
+
+    has 'name' => (
+        is => 'rw',
+        isa => 'Str',
+        traits => [ qw(MooseX::Meta::Attribute::Searchable) ],
+        search_field_names => [ qw(name name_ngram) ],
+    );
+
+    1;
+
+    # Later...
+
+    my $obj = MyObject->new(name => 'user');
+    my $href = $obj->get_searchable_hashref;
+    $searchengine->index(id => $obj->id, $href);
+
+=head1 DESCRIPTION
+
+MooseX::Meta::Attribute::Searchable and L<MooseX::Role::Searchable> combine to
+to mark attributes for indexing, naming any alternative names it may use, and
+to create a datastructure suitable for passing to an external indexer.
+
+By applying the MooseX::Meta::Attribute::Searchable trait to an attribute, you
+are signaling that the attribute will be included in the index.
+
+If you specify C<search_field_names> then copies of the attribute will be
+included in your result for populating other fields.
+
+=begin :prelude
+
+=head1 MOTIVATION
+
+In many of my projects an external search index such as
+L<Solr|http://lucene.apache.org/solr/> or
+L<ElasticSearch|http://www.elasticsearch.org/> is used.  Many times the things
+I am indexing are Moose objects.  This pair of roles makes it easy to mark
+which attributes should be included in the indexed document and optionally
+gives the ability to rename or specificy multiple names for an attribute.
+
+The optional <search_field_names> is useful when you have a single value that
+you'd like to index in multiple ways.  An example of this is a field that you
+might want searchable in both full-text and autocomplete situations.  In Solr
+I've used a "name" and "name_ngram" field in the schema, where the latter used
+an EdgeNGram filter for auto-complete.
+
+=end :prelude
+
+=attr search_field_names
+
+The field name(s) in the search index.  Even if you only use one name, this
+must be an ArrayRef.
+
+=method has_search_field_names
+
+Returns true if this attribute has any field names specified.
+
+=cut
+
+has 'search_field_names' => (
+    is => 'ro',
+    isa => 'ArrayRef[Str]',
+    predicate => 'has_search_field_names',
+);
+
+no Moose::Role;
 1;
---
+++ lib/MooseX/Role/Searchable.pm
@@ -0,0 +1,77 @@
+package MooseX::Role::Searchable;
+use Moose::Role;
+
+requires 'pack';
+
+# ABSTRACT: Add search convenience methods to a class.
+
+=head1 SYNOPSIS
+
+    package MyObject;
+    use Moose;
+
+    with 'MooseX::Role::Searchable';
+
+    has 'name' => (
+        is => 'rw',
+        isa => 'Str',
+        traits => [ qw(MooseX::Meta::Attribute::Searchable) ],
+        search_field_names => [ qw(name name_ngram) ],
+    );
+
+    1;
+
+    # Later...
+
+    my $obj = MyObject->new(name => 'user');
+    my $href = $obj->get_searchable_hashref;
+    $searchengine->index(id => $obj->id, $href);
+
+=head1 DESCRIPTION
+
+When used in conjuntion with L<MooseX::Meta::Attribute::Searchable>, this role
+can create HashRefs containing all the data necessary to index an object in
+a search index.
+
+B<NOTE:> This role expects that it's consuming class will have a C<pack>
+method that will return a HashRef, like the one provided by L<MooseX::Storage>.
+
+=method get_searchable_hashref
+
+Returns a hashref with values for all of the search field names that were
+specified on the various attributes with the Searchable trait.
+
+=cut
+
+sub get_searchable_hashref {
+    my ($self) = @_;
+
+    my $meta = $self->meta;
+
+    # Get the hashref that was so lovingly enabled with MX::Storage
+    my $href = $self->pack;
+    my $ret = ();
+
+    for my $attr ($meta->get_all_attributes) {
+        my $name = $attr->name;
+        next if $name eq '_trait_namespace' || $name eq 'id';
+        next unless $href->{$name};
+
+        my $field_name = $name;
+
+        if($attr->does('MooseX::Meta::Attribute::Searchable')) {
+            # Either snag the names or use the attribute's name
+            my $names = $attr->has_search_field_names ? $attr->search_field_names : [ $name ];
+            foreach my $fname (@{ $names }) {
+                $ret->{$fname} = $href->{$name};
+            }
+        }
+
+    }
+
+    return $ret;
+}
+
+no Moose::Role;
+
+1;
